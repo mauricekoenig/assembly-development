23b135fe887af62957e13ca18045f6bc53a2e203
Maurice <60366053+mauricekoenig@users.noreply.github.com>
2026-09-24 01:56:07 +0200

    Add files via upload

---
+++ Guard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public static class Guard
+{
+    public static void NotNull(
+        object value)
+    {
+        if (value == null)
+            throw new ArgumentNullException();
+    }
+
+    public static void NotNull(
+        object value,
+        string paramName)
+    {
+        if (value == null)
+            throw new ArgumentNullException(paramName);
+    }
+
+    public static void NotNullOrWhiteSpace(
+        string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException(
+                "Value cannot be null or whitespace.");
+    }
+
+    public static void NotNullOrWhiteSpace(
+        string value,
+        string paramName)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException(
+                "Value cannot be null or whitespace.",
+                paramName);
+    }
+}
